f55e036bac1ffa914ae1b8785efb87e52e89b27d
Pedro <pedropadilha15.pp@gmail.com>
2023-07-03 23:08:44 -0300

    first commit

---
+++ Send_Data_Arduino/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Send_Data_Arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_ON = new System.Windows.Forms.Button();
+            this.btn_OFF = new System.Windows.Forms.Button();
+            this.SerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_ON
+            // 
+            this.btn_ON.Location = new System.Drawing.Point(139, 106);
+            this.btn_ON.Name = "btn_ON";
+            this.btn_ON.Size = new System.Drawing.Size(107, 58);
+            this.btn_ON.TabIndex = 0;
+            this.btn_ON.Text = "ON LED";
+            this.btn_ON.UseVisualStyleBackColor = true;
+            this.btn_ON.Click += new System.EventHandler(this.btn_ON_Click);
+            // 
+            // btn_OFF
+            // 
+            this.btn_OFF.Location = new System.Drawing.Point(252, 106);
+            this.btn_OFF.Name = "btn_OFF";
+            this.btn_OFF.Size = new System.Drawing.Size(107, 58);
+            this.btn_OFF.TabIndex = 1;
+            this.btn_OFF.Text = "OFF LED";
+            this.btn_OFF.UseVisualStyleBackColor = true;
+            this.btn_OFF.Click += new System.EventHandler(this.btn_OFF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 271);
+            this.Controls.Add(this.btn_OFF);
+            this.Controls.Add(this.btn_ON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ON;
+        private System.Windows.Forms.Button btn_OFF;
+        private System.IO.Ports.SerialPort SerialPort;
+    }
+}
+
---
+++ Send_Data_Arduino/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+
+namespace Send_Data_Arduino
+{
+    public partial class Form1 : Form
+    {
+        //CRIA OBJETO DE CONEXÃO COM O ARDUINO
+        SerialPort conexao = new SerialPort("COM3", 9600);
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            //CONFIGURANDO COR DE FUNDO DOS BOTÕES
+            btn_OFF.BackColor = Color.White;
+            btn_ON.BackColor = Color.White;
+        }
+
+        //BOTAO LIGA LED
+        private void btn_ON_Click(object sender, EventArgs e)
+        {
+            conexao.Open();         //ABRE CONEXAO
+            Thread.Sleep(500);      //DELAY
+            if (conexao.IsOpen)
+            {
+                //ENVIA COMANDO "DIGC1" PARA ARDUINO - LIGA LED
+                conexao.Write("L1");
+            }
+            //string retorno = conexao.ReadLine();
+            Thread.Sleep(500);      //DELAY
+            conexao.Close();        //FECHA CONEXÃO
+
+            btn_ON.BackColor= Color.Green;
+            btn_OFF.BackColor= Color.White;
+        }
+
+        //BOTAO DESLIGA LED
+        private void btn_OFF_Click(object sender, EventArgs e)
+        {
+            conexao.Open();         //ABRE CONEXAO
+            Thread.Sleep(500);      //DELAY
+            if (conexao.IsOpen)
+            {
+                //ENVIA COMANDO "DIGC0" PARA ARDUINO - DESLIGA LED
+                conexao.Write("L0");
+            }
+            //string retorno = conexao.ReadLine();
+            Thread.Sleep(500);      //DELAY
+            conexao.Close();        //FECHA CONEXAO
+            btn_ON.BackColor = Color.White;
+            btn_OFF.BackColor = Color.Red;
+        }
+    }
+}
